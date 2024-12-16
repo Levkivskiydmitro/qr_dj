bbdbb1224538ceabca1849768f6348cb1ef30e0d
Levkivskiydmitro <dimon65479@gmail.com>
2024-12-16 20:22:50 +0200

    commit

--- qr/reg/apps.py
+++ qr/reg/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class RegConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reg'
+
+import flask
+
+registration = flask.Blueprint(
+    name="registration",
+    import_name="app",
+    template_folder="registration/templates"
+)
--- qr/reg/views.py
+++ qr/reg/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def render_reg(request):
     return render(request, 'reg.html')
+
+import flask
+from .models import User, db
+
+def render_register():
+    is_registered = False
+    if flask.request.method == 'POST':
+        user = User(
+            username = flask.request.form['username'],
+            password = flask.request.form['password'],
+        )
+        try:
+            db.session.add(user)
+            db.session.commit()
+            is_registered = True
+            
+            return flask.redirect('/auth')
+        except Exception as e:
+            is_registered = False
+            return f"{e}" 
+    return flask.render_template(template_name_or_list="registration.html", is_registered = is_registered)
