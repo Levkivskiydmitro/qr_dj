d0a3258666883f46046155db5954878a465a774e
Levkivskiydmitro <dimon65479@gmail.com>
2024-12-16 20:25:22 +0200

    commit

--- qr/reg/models.py
+++ qr/reg/models.py
@@ -1,23 +1,2 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length= 255)
-    email = models.CharField(max_length= 255)
-    date_of_birth = email = models.CharField(max_length= 255)
-
-    def __repr__(self) -> str:
-        return f"id: {self.id}"
-
-
-
-from main.settings import db
-from flask_login import UserMixin
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer(), primary_key = True)
-    username = db.Column(db.Text, nullable=False)
-    password = db.Column(db.Text, nullable=False)
-
-    def __repr__(self) -> str:
-        return f"id: {self.id}"
